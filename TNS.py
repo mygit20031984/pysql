f83331941d6e643e2dd71a5fa0286292d6133f1d
mygit20031984 <ashuklkrn@gmail.com>
2020-08-27 17:06:57 +0530

    commit

--- TNS.py
+++ TNS.py
@@ -2,3 +2,4 @@ def TNS1(df1, df2, run_id, parent_dir):
     df_tns = df2.merge(df1, how='outer', indicator=True).query('_merge == "left_only"').drop('_merge', 1)
     output_file_name = parent_dir + '/Target_not_in_Source' + run_id + '.csv'
     df_tns.to_csv(output_file_name, index=False)
+    return len(df_tns.index)
--- read_csv.py
+++ read_csv.py
@@ -6,46 +6,60 @@ import find_dup
 from datetime import datetime
 import os
 
-###
-# 1. Source Records Not In Target
-# 2. Target Records Not in Resource
-# 3. Perfect Columns
-# 4. Overall TestCase Status
-# 5. Duplicate In Source
-# 6. Duplicate In Target
-###
-
-
-# Setting Run ID & PArent Folder#
+# Test
+#############################################
+# Setting up Parent Folder and Run_ID
+#############################################
 run_id = datetime.now().strftime("%Y%m%d_%H%M%S")
 parent_dir = "C:/Users/Owner/PycharmProjects/pysql/Results/Report_" + run_id
 print(parent_dir)
 os.makedirs(parent_dir)
-print("RunID and Parent Dir is Set")
+print("RunID and Parent Dir is Created")
 
 # Reading Input File#
 xl = pd.ExcelFile("input_file.xlsx")
 df = xl.parse("Sheet1")
 x1 = df['Value'][1]
-x2 = df['Value'][4]
+x2 = df['Value'][6]
 
 # Creating 2 Dataframes#
 df1 = pd.read_csv(x1)
 df2 = pd.read_csv(x2)
 
-# print("Find Duplicate records in DF1")
-find_dup.find_dup1(df1, parent_dir, run_id)
+#############################################
+# Find Duplicate from Data Sets
+#############################################
+# print("Find Duplicate records in Data Sets")
+df1_dup = find_dup.find_dup1(df1, 'Source', parent_dir, run_id)
+df2_dup = find_dup.find_dup1(df2, 'Target', parent_dir, run_id)
 
-# print("Compare Data Sets")
-# Compare 2 data sets#
-# compare.com1(df1, df2, run_id, parent_dir)
+print("Number of Duplicate Records in DF1  = " + str(df1_dup))
+print("Number of Duplicate Records in DF2  = " + str(df2_dup))
 
+#############################################
+# Remove Duplicate from Source and Target Data Sets
+#############################################
+print("Removing Duplicate records in Data Sets")
+df1 = df1.sort_values("ID", inplace=False)
+df1 = df1.drop_duplicates(subset="ID", keep='first', inplace=False)
 
-#
-# print("Executing Data available in Source and not in Target")
-# # Find Records available in Source and not in Target#
-# SNT.SNT1(df1, df2, run_id, parent_dir)
-#
-# print("Executing Data available in Target and not in Source")
-# # Find Records available in Target and not in Source#
-# TNS.TNS1(df1, df2, run_id, parent_dir)
+df2 = df2.sort_values("ID", inplace=False)
+df2 = df2.drop_duplicates(subset="ID", keep='first', inplace=False)
+
+# #####################################################
+# #Records available in Source Data Sets not in Target
+# #####################################################
+cnt_SNT = SNT.SNT1(df1, df2, run_id, parent_dir)
+print("Number of Records in Source and not in Target = " + str(cnt_SNT))
+
+# #####################################################
+# #Records available in Target Data Sets not in Source
+# #####################################################
+cnt_TNS = TNS.TNS1(df1, df2, run_id, parent_dir)
+print("Number of Records in Source and not in Target = " + str(cnt_TNS))
+
+# #####################################################
+# #Compare Source and Target Data Sets
+# #####################################################
+print("Compare Data Sets")
+compare.com1(df1, df2, run_id, parent_dir)
